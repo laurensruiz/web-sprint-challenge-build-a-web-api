110fa325cb17b3f237531b1bff4eb46a1ef29b42
laurensruiz <laurenruiz97@yahoo.com>
2022-05-13 00:41:30 -0400

    did get for proj router

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,27 @@
 // Write your "projects" router here!
+const express = require('express')
+const Project = require('./projects-model')
+
+const router = express.Router()
+//
+router.get('/', (req, res, next) => {
+    // RETURN AN ARRAY WITH ALL THE USERS
+    Project.get(req.params.id)
+    .then(users => {
+      res.json(users)
+    })
+    .catch(next)
+  });
+
+//error handling middleware
+
+router.use((err, req, res, next) => { //eslint-disable-line
+    res.status(err.status || 500).jsson({
+      customMessage: 'something really bad happened in the router',
+      message: err.message,
+      stack: err.stack,
+    })
+  })
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const server = express();
 
 //need to add router and middleware here
+const projRouter = require('./projects/projects-router')
 
 server.use(express.json())
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+server.use('/api/projects', projRouter)
 
 module.exports = server;
