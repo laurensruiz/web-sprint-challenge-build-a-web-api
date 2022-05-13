374861cd42ec8e7951ca5a965492d8e2751628e7
laurensruiz <laurenruiz97@yahoo.com>
2022-05-13 00:26:17 -0400

    started making server

--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const server = express();
 
+//need to add router and middleware here
+
+server.use(express.json())
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
--- index.js
+++ index.js
@@ -12,3 +12,15 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+
+require('dotenv').config()
+const server = require('./api/server')
+const PORT = process.env.PORT //|| 9000; // allows port to change
+
+if(PORT == null) {
+    console.error('no port set')
+} else {
+    server.listen(PORT, () => {
+        console.log(`server is running on port ${PORT}`)
+    })
+}
